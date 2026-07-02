a5c63575d394fe0e8706ec4f8fbdbac2df3d42af
Dudhade Shital <137904313+shital0101@users.noreply.github.com>
2026-07-02 14:48:27 +0530

    Add files via upload

---
+++ areaofrect.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int len,breadth;
+
+	cout<<"Enter the lenght:";
+	cin>>len;
+	cout<<endl;
+	cout<<"Enter the Breadth:";
+	cin>>breadth;
+
+	int area=len*breadth;
+	cout<<"Area of Rectangle:"<<area<<endl;
+
+}
