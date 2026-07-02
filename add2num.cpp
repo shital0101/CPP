4e57cf6191f74c5e129a9dc403cda1464e99e69b
Dudhade Shital <137904313+shital0101@users.noreply.github.com>
2026-07-02 14:48:04 +0530

    Add files via upload

---
+++ add2num.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a,b;
+	cout<<"Enter the First Number"<<endl;
+	cin>>a;
+
+	cout<<"Enter the Second Number"<<endl;
+	cin>>b;
+
+	int c=a+b;
+	cout<<"Addition of two numbers is::"<<c<<endl;
+}
