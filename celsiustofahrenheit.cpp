5d4f58b65db8ace5109a130cc8cd63130c8a2f8e
Dudhade Shital <137904313+shital0101@users.noreply.github.com>
2026-07-03 12:00:13 +0530

    Add files via upload

---
+++ celsiustofahrenheit.cpp
@@ -0,0 +1,16 @@
+//Convert Celsius to Fahrenheit.
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int celsius;
+
+	cout<<"Enter the Celsius::";
+	cin>>celsius;
+
+	int fahrenheit=(celsius*1.8)+32;
+
+	cout<<"The Fahrenheit is::"<<fahrenheit<<endl;
+
+}
