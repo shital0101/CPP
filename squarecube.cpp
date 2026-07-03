a252b70afcdc682fb70178b72f725e3d32964291
Dudhade Shital <137904313+shital0101@users.noreply.github.com>
2026-07-03 12:00:39 +0530

    Add files via upload

---
+++ squarecube.cpp
@@ -0,0 +1,17 @@
+//find the square and cube of a number.
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int num;
+	cout<<"Enter the Number::";
+	cin>>num;
+
+	int sqr=num*num;
+	cout<<"Square of number="<<sqr<<endl;
+
+	int cube=num*num*num;
+	cout<<"Cube of Number="<<cube<<endl;
+
+}
