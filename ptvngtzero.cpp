608de14034886f1e257f986f31f3da01d033914c
Dudhade Shital <137904313+shital0101@users.noreply.github.com>
2026-07-04 17:30:30 +0530

    Add files via upload

---
+++ ptvngtzero.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int num;
+	cout<<"Enter the number::";
+	cin>>num;
+
+	if(num>0)
+	{
+	     cout<<"Number is Positive"<<endl;
+	}
+	else if(num<0)
+	{
+	     cout<<"Number is Negative"<<endl;
+	}
+	else
+	{
+	    cout<<"Number is zero"<<endl;
+	}
+
+}
