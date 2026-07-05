7946be3289d87fbcc1f82da7a28719b1f104304f
Dudhade Shital <137904313+shital0101@users.noreply.github.com>
2026-07-05 10:36:21 +0530

    Add files via upload

---
+++ evenodd.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int num;
+
+	cout<<"Enter the Number::";
+	cin>>num;
+
+	if(num%2==0)
+	{
+		cout<<"Number is Even..."<<endl;
+	}
+	else
+	{
+		cout<<"Number is Odd..."<<endl;
+	}
+}
