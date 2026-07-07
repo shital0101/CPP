b72cb5f7cbe75327f1dc8e2579a992cecdf5e7be
Dudhade Shital <137904313+shital0101@users.noreply.github.com>
2026-07-07 11:48:04 +0530

    Add files via upload

---
+++ num1ton.cpp
@@ -0,0 +1,18 @@
+//Print numbers from 1 to n.
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n,i;
+
+	cout<<"Enter the limit to print the Numbers:";
+	cin>>n;
+
+	cout<<"Numbers from 1 to "<<n<<endl;
+	for(i=1;i<=n;i++)
+	{
+	    cout<<i<<" ";
+	}
+
+}
