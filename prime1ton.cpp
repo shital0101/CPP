2f7113efde9dd52c714722df82c8fc1076a73c62
Dudhade Shital <137904313+shital0101@users.noreply.github.com>
2026-07-16 22:07:42 +0530

    Add files via upload

---
+++ prime1ton.cpp
@@ -0,0 +1,38 @@
+//Print the prime number from 1 to n
+#include<iostream>
+using namespace std;
+
+bool isprime(int n)
+{
+	if(n==1)
+	   return false;
+	for(int i=2;i<n;i++)
+	{
+	   if(n%i==0)
+	   {
+		return false;
+	   }
+	}
+
+	return true;
+}
+
+
+int main()
+{
+	int i,n,flag=0;
+
+	cout<<"Enter the limit:";
+	cin>>n;
+
+	cout<<"Prime numbers from 1 to "<<n<<endl;
+
+	for(i=1;i<=n;i++)
+	{
+	   if(isprime(i))
+	   {
+		cout<<i<<" ";
+	   }
+	}
+
+}
